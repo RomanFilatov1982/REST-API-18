3f9723bfa8892f1bd1933648052115e3a2c3f3b2
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-09-11 13:47:56 +0300

    add Unversioned Files

--- src/test/java/demoqa/tests/TestData.java
+++ src/test/java/demoqa/tests/TestData.java
@@ -3,8 +3,8 @@ package demoqa.tests;
 import com.codeborne.selenide.Configuration;
 
 public class TestData {
-    public static String login = System.getProperty("login");
-    public static String password = System.getProperty("password");
+    public static final String login = System.getProperty("login");
+    public static final String password = System.getProperty("password");
 
     /*public static String login = "test123456",
             password = "Test123456@";*/
