c43472371fd7cb0fd264f7dd88245eef528e5225
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-09-19 15:28:11 +0300

    change baseUri

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -18,12 +18,19 @@ public class TestBase {
 
     @BeforeAll
     static void setup() {
-        //Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com/");
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com/");
         RestAssured.baseURI = System.getProperty("baseUrl", "https://demoqa.com/");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "138.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.remote = System.getProperty("remoteUrl");
+
+
+       /* Configuration.pageLoadTimeout = 60000;
+        Configuration.baseUrl = "https://sport-marafon.ru/";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;*/
         //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
